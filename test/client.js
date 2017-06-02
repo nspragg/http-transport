1ac9d5a30c14e2685054abff725a854312a72e92
nick spragg <nick.spragg@bbc.co.uk>
2017-06-02 15:18:04 +0100

    Feature: Add basic support for POST

--- test/client.js
+++ test/client.js
@@ -19,10 +19,10 @@ const api = nock(host);
 const path = '/';
 
 const simpleResponseBody = 'Illegitimi non carborundum';
-
-const responseBody = {
+const requestBody = {
   foo: 'bar'
 };
+const responseBody = requestBody;
 
 function assertResponse(response, expected) {
   assert.ok(expected);
@@ -85,247 +85,262 @@ describe('Blackadder', () => {
       }, TypeError, 'Plugin is not a function');
     });
   });
-});
 
-describe('.headers', () => {
-  it('sends a custom headers', () => {
-    nock.cleanAll();
-
-    const HeaderValue = `${packageInfo.name}/${packageInfo.version}`;
-    nock(host, {
-        reqheaders: {
-          'User-Agent': HeaderValue,
-          foo: 'bar'
-        }
-      })
-      .get(path)
-      .reply(200, responseBody);
-
-    const response = Blackadder.createClient()
-      .get(url)
-      .headers({
-        'User-Agent': HeaderValue,
-        foo: 'bar'
-      })
-      .asResponse();
-
-    return response
-      .catch(assert.ifError)
-      .then((res) => {
-        assert.equal(res.statusCode, 200);
-      });
-  });
-
-  it('asserts for a missing header', () => {
-    assert.throws(() => {
-      Blackadder.createClient()
-        .get(url)
-        .headers();
-    }, Error, 'missing headers');
-  });
-
-  it('asserts an empty header object', () => {
-    assert.throws(() => {
-      Blackadder.createClient()
-        .get(url)
-        .headers({});
-    }, Error, 'missing headers');
-  });
-});
-
-describe('query strings', () => {
-  it('supports adding a query string', function () {
-    api.get('/?a=1').reply(200, simpleResponseBody);
-
-    const client = Blackadder.createClient();
-    return client
-      .get(url)
-      .query('a', 1)
-      .asBody()
-      .then((body) => {
-        assert.equal(body, simpleResponseBody);
-      });
-  });
-
-  it('supports multiple query strings', function () {
-    nock.cleanAll();
-    api.get('/?a=1&b=2&c=3').reply(200, simpleResponseBody);
-
-    const client = Blackadder.createClient();
-    return client
-      .get(url)
-      .query({
-        'a': 1,
-        'b': 2,
-        'c': 3,
-      })
-      .asBody()
-      .then((body) => {
-        assert.equal(body, simpleResponseBody);
-      });
-  });
-
-  it('asserts empty query strings object', () => {
-    assert.throws(() => {
-      Blackadder.createClient()
-        .get(url)
-        .query({});
-    }, Error, 'missing query strings');
-  });
-});
-
-describe('plugins', () => {
-  it('supports a per request plugin', () => {
-    nock.cleanAll();
-    api.get(path).times(2).reply(200, simpleResponseBody);
-
-    const toUpperCase = () => {
-      return (ctx, next) => {
-        return next().then(() => {
-          ctx.res.body = ctx.res.body.toUpperCase();
-        });
-      };
-    };
-    const client = Blackadder.createClient();
-
-    const upperCaseResponse = client
-      .use(toUpperCase())
-      .get(url)
-      .asBody();
-
-    const lowerCaseResponse = client
-      .get(url)
-      .asBody();
-
-    return Promise.all([upperCaseResponse, lowerCaseResponse])
-      .then((results) => {
-        assert.equal(results[0], simpleResponseBody.toUpperCase());
-        assert.equal(results[1], simpleResponseBody);
-      });
-  });
-
-  it('throws if a global plugin is not a function', () => {
-    assert.throws(() => {
-      Blackadder.createClient()
-        .useGlobal('bad plugin')
-        .headers();
-    }, TypeError, 'Plugin is not a function');
-  });
-
-  it('throws if a per request plugin is not a function', () => {
-    assert.throws(() => {
-      const client = Blackadder.createClient();
-      client
-        .use('bad plugin')
-        .get(url);
-    }, TypeError, 'Plugin is not a function');
-  });
-
-  describe('toJson', () => {
-    it('returns body of a JSON response', () => {
-      nock.cleanAll();
-      api.get(path).reply(200, responseBody);
+  describe('.post', () => {
+    it('makes a POST request', () => {
+      api.post(path, requestBody).reply(201, responseBody);
 
       const client = Blackadder.createClient();
-      client.useGlobal(toJson());
-
       return client
-        .get(url)
+        .post(url, requestBody)
         .asBody()
         .then((body) => {
-          assert.equal(body.foo, 'bar');
-        });
+          assert.deepEqual(body, responseBody);
+        })
+        .catch(assert.ifError);
     });
   });
 
-  describe('timeout', () => {
-    it('sets the a timeout', () => {
+  describe('.headers', () => {
+    it('sends a custom headers', () => {
       nock.cleanAll();
-      api.get('/')
-        .socketDelay(1000)
-        .reply(200, simpleResponseBody);
 
-      const client = Blackadder.createClient();
-      const response = client
-        .useGlobal(timeout(20))
+      const HeaderValue = `${packageInfo.name}/${packageInfo.version}`;
+      nock(host, {
+          reqheaders: {
+            'User-Agent': HeaderValue,
+            foo: 'bar'
+          }
+        })
+        .get(path)
+        .reply(200, responseBody);
+
+      const response = Blackadder.createClient()
         .get(url)
-        .asBody();
+        .headers({
+          'User-Agent': HeaderValue,
+          foo: 'bar'
+        })
+        .asResponse();
 
-      return assertFailure(response, 'Request failed for http://www.example.com/ ESOCKETTIMEDOUT');
-    });
-
-    it('default timeout?');
-  })
-
-  describe('logging', () => {
-    it('logs each request at info level when a logger is passed in', () => {
-      api.get(path).reply(200);
-
-      const stubbedLogger = {
-        info: sandbox.stub(),
-        warn: sandbox.stub()
-      };
-
-      return Blackadder.createClient()
-        .get(url)
-        .useGlobal(log(stubbedLogger))
-        .asBody()
+      return response
         .catch(assert.ifError)
-        .then(() => {
-          const message = stubbedLogger.info.getCall(0).args[0];
-          assert.match(message, /GET http:\/\/www.example.com\/ 200 \d+ ms/);
+        .then((res) => {
+          assert.equal(res.statusCode, 200);
         });
+    });
+
+    it('asserts for a missing header', () => {
+      assert.throws(() => {
+        Blackadder.createClient()
+          .get(url)
+          .headers();
+      }, Error, 'missing headers');
+    });
+
+    it('asserts an empty header object', () => {
+      assert.throws(() => {
+        Blackadder.createClient()
+          .get(url)
+          .headers({});
+      }, Error, 'missing headers');
     });
   });
 
-  describe('toError', () => {
-    it('returns an error for a non 200 response', () => {
-      nock.cleanAll();
-      api.get(path).reply(500);
-
-      const client = Blackadder.createClient();
-      const response = client
-        .useGlobal(toError())
-        .get(url)
-        .asBody();
-
-      return assertFailure(response, 'Received HTTP code 500 for GET http://www.example.com/')
-    });
-
-    it('includes the status code in the error for a non 200 response', () => {
-      nock.cleanAll();
-      api.get(path).reply(500);
+  describe('query strings', () => {
+    it('supports adding a query string', function () {
+      api.get('/?a=1').reply(200, simpleResponseBody);
 
       const client = Blackadder.createClient();
       return client
         .get(url)
+        .query('a', 1)
         .asBody()
-        .catch((err) => {
-          assert(err);
-          assert.equal(err.statusCode, 500);
+        .then((body) => {
+          assert.equal(body, simpleResponseBody);
         });
     });
 
-    it('includes the headers in the error for a non 200 response', function () {
+    it('supports multiple query strings', function () {
       nock.cleanAll();
-      api.get(path).reply(500, {
-        error: 'this is the body of the error'
-      }, {
-        'www-authenticate': 'Bearer realm="/"'
-      });
+      api.get('/?a=1&b=2&c=3').reply(200, simpleResponseBody);
 
       const client = Blackadder.createClient();
-      const response = client
-        .useGlobal(toError())
+      return client
+        .get(url)
+        .query({
+          'a': 1,
+          'b': 2,
+          'c': 3,
+        })
+        .asBody()
+        .then((body) => {
+          assert.equal(body, simpleResponseBody);
+        });
+    });
+
+    it('asserts empty query strings object', () => {
+      assert.throws(() => {
+        Blackadder.createClient()
+          .get(url)
+          .query({});
+      }, Error, 'missing query strings');
+    });
+  });
+
+  describe('plugins', () => {
+    it('supports a per request plugin', () => {
+      nock.cleanAll();
+      api.get(path).times(2).reply(200, simpleResponseBody);
+
+      const toUpperCase = () => {
+        return (ctx, next) => {
+          return next().then(() => {
+            ctx.res.body = ctx.res.body.toUpperCase();
+          });
+        };
+      };
+      const client = Blackadder.createClient();
+
+      const upperCaseResponse = client
+        .use(toUpperCase())
         .get(url)
         .asBody();
 
-      return response
-        .then(() => assert.ok(false, 'Promise should have failed'))
-        .catch((err) => {
-          assert(err);
-          assert.equal(err.headers['www-authenticate'], 'Bearer realm="/"');
+      const lowerCaseResponse = client
+        .get(url)
+        .asBody();
+
+      return Promise.all([upperCaseResponse, lowerCaseResponse])
+        .then((results) => {
+          assert.equal(results[0], simpleResponseBody.toUpperCase());
+          assert.equal(results[1], simpleResponseBody);
         });
+    });
+
+    it('throws if a global plugin is not a function', () => {
+      assert.throws(() => {
+        Blackadder.createClient()
+          .useGlobal('bad plugin')
+          .headers();
+      }, TypeError, 'Plugin is not a function');
+    });
+
+    it('throws if a per request plugin is not a function', () => {
+      assert.throws(() => {
+        const client = Blackadder.createClient();
+        client
+          .use('bad plugin')
+          .get(url);
+      }, TypeError, 'Plugin is not a function');
+    });
+
+    describe('toJson', () => {
+      it('returns body of a JSON response', () => {
+        nock.cleanAll();
+        api.get(path).reply(200, responseBody);
+
+        const client = Blackadder.createClient();
+        client.useGlobal(toJson());
+
+        return client
+          .get(url)
+          .asBody()
+          .then((body) => {
+            assert.equal(body.foo, 'bar');
+          });
+      });
+    });
+
+    describe('timeout', () => {
+      it('sets the a timeout', () => {
+        nock.cleanAll();
+        api.get('/')
+          .socketDelay(1000)
+          .reply(200, simpleResponseBody);
+
+        const client = Blackadder.createClient();
+        const response = client
+          .useGlobal(timeout(20))
+          .get(url)
+          .asBody();
+
+        return assertFailure(response, 'Request failed for http://www.example.com/ ESOCKETTIMEDOUT');
+      });
+
+      it('default timeout');
+    })
+
+    describe('logging', () => {
+      it('logs each request at info level when a logger is passed in', () => {
+        api.get(path).reply(200);
+
+        const stubbedLogger = {
+          info: sandbox.stub(),
+          warn: sandbox.stub()
+        };
+
+        return Blackadder.createClient()
+          .get(url)
+          .useGlobal(log(stubbedLogger))
+          .asBody()
+          .catch(assert.ifError)
+          .then(() => {
+            const message = stubbedLogger.info.getCall(0).args[0];
+            assert.match(message, /GET http:\/\/www.example.com\/ 200 \d+ ms/);
+          });
+      });
+    });
+
+    describe('toError', () => {
+      it('returns an error for a non 200 response', () => {
+        nock.cleanAll();
+        api.get(path).reply(500);
+
+        const client = Blackadder.createClient();
+        const response = client
+          .useGlobal(toError())
+          .get(url)
+          .asBody();
+
+        return assertFailure(response, 'Received HTTP code 500 for GET http://www.example.com/')
+      });
+
+      it('includes the status code in the error for a non 200 response', () => {
+        nock.cleanAll();
+        api.get(path).reply(500);
+
+        const client = Blackadder.createClient();
+        return client
+          .get(url)
+          .asBody()
+          .catch((err) => {
+            assert(err);
+            assert.equal(err.statusCode, 500);
+          });
+      });
+
+      it('includes the headers in the error for a non 200 response', function () {
+        nock.cleanAll();
+        api.get(path).reply(500, {
+          error: 'this is the body of the error'
+        }, {
+          'www-authenticate': 'Bearer realm="/"'
+        });
+
+        const client = Blackadder.createClient();
+        const response = client
+          .useGlobal(toError())
+          .get(url)
+          .asBody();
+
+        return response
+          .then(() => assert.ok(false, 'Promise should have failed'))
+          .catch((err) => {
+            assert(err);
+            assert.equal(err.headers['www-authenticate'], 'Bearer realm="/"');
+          });
+      });
     });
   });
 });
