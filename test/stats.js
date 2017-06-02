4161ac7b432e3272e47d9d059985dd5681206e7e
Catriona Morrison <catrionajanemorrison@hotmail.com>
2017-06-02 11:42:11 +0100

    first stats test

--- test/stats.js
+++ test/stats.js
@@ -1,1 +1,33 @@
-#!/usr/bin/env bash
+'use strict';
+
+const sinon = require('sinon');
+const nock = require('nock');
+const assert = require('chai').assert;
+const sandbox = sinon.sandbox.create();
+
+const Blackadder = require('..');
+const stats = require('../lib/plugins/stats');
+
+const host = 'http://www.example.com';
+const url = 'http://www.example.com/';
+const api = nock(host);
+
+describe('stats', () => {
+    it('increments counter http.requests for each request', () => {
+        api.get('/').reply(200);
+
+        const stubbedStats = {
+            increment: sandbox.stub(),
+            timing: sandbox.stub()
+        };
+
+        return Blackadder.createClient()
+            .get(url)
+            .use(stats(stubbedStats))
+            .asBody()
+            .catch(assert.ifError)
+            .then(() => {
+                sinon.assert.calledWith(stubbedStats.increment, 'http.requests');
+            });
+    });
+});
