60cd68cfc37836637e7b28825cf885192accc48c
nick spragg <nick.spragg@bbc.co.uk>
2017-06-07 19:29:28 +0100

    Refactor: Renaming refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var Client = require('./lib/client');
+const Client = require('./lib/client');
+const DEFAULT_REQUEST_FACTORY = require('./lib/transport/requestClient');
 
-module.exports.createClient = () => {
-  return new Client();
+module.exports.createClient = (httpRequestFactory) => {
+  return new Client(httpRequestFactory || DEFAULT_REQUEST_FACTORY);
 };
