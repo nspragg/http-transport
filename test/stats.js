61ff3a8ab20a9d3938904caf1ff23e1f99669f8c
Catriona Morrison <catrionajanemorrison@hotmail.com>
2017-06-07 16:37:18 +0100

    formatting

--- test/stats.js
+++ test/stats.js
@@ -12,67 +12,61 @@ const host = 'http://www.example.com';
 const url = 'http://www.example.com/';
 const api = nock(host);
 const stubbedStats = {
-    increment: sandbox.stub(),
-    timing: sandbox.stub()
+  increment: sandbox.stub(),
+  timing: sandbox.stub()
 };
 
 describe('stats', () => {
-    beforeEach(() => {
-        api.get('/').reply(200);
-    });
 
-    it('increments counter http.requests for each request', () => {
-        return Blackadder.createClient()
-            .get(url)
-            .use(stats(stubbedStats))
-            .asBody()
-            .catch(assert.ifError)
-            .then(() => {
-                sinon.assert.calledWith(stubbedStats.increment, 'http.requests');
-            });
-    });
+  it('increments counter http.requests for each request', () => {
+    api.get('/').reply(200);
 
-    it('increments counter a request counter with the name of the client if one is provided', () => {
-        return Blackadder.createClient()
-            .get(url)
-            .use(stats(stubbedStats, 'my-client'))
-            .asBody()
-            .catch(assert.ifError)
-            .then(() => {
-                sinon.assert.calledWith(stubbedStats.increment, 'my-client.requests');
-            });
-    });
+    return Blackadder.createClient()
+      .get(url)
+      .use(stats(stubbedStats))
+      .asBody()
+      .catch(assert.ifError)
+      .then(() => {
+        sinon.assert.calledWith(stubbedStats.increment, 'http.requests');
+      });
+  });
 
-    it('increments a request counter with the name of the client and feed if provided', () => {
-        return Blackadder.createClient()
-            .get(url)
-            .use(stats(stubbedStats, 'my-client', 'feedName'))
-            .asBody()
-            .catch(assert.ifError)
-            .then(() => {
-                sinon.assert.calledWith(stubbedStats.increment, 'my-client.feedName.requests');
-            });
-    });
+  it('increments counter a request counter with the name of the client if one is provided', () => {
+    api.get('/').reply(200);
 
-    it('increments counter response for each response', () => {
-        return Blackadder.createClient()
-            .get(url)
-            .use(stats(stubbedStats, 'my-client', 'feedName'))
-            .asBody()
-            .catch(assert.ifError)
-            .then(() => {
-                sinon.assert.calledWith(stubbedStats.increment, 'my-client.feedName.responses.200');
-            });
-    });
+    return Blackadder.createClient()
+      .get(url)
+      .use(stats(stubbedStats, 'my-client'))
+      .asBody()
+      .catch(assert.ifError)
+      .then(() => {
+        sinon.assert.calledWith(stubbedStats.increment, 'my-client.requests');
+      });
+  });
 
-    it('records a timer for the response time', () => {
-        return Blackadder.createClient()
-            .get(url)
-            .use(stats(stubbedStats, 'my-client', 'feedName'))
-            .asBody()
-            .catch(assert.ifError)
-            .then(() => {
-                sinon.assert.calledWith(stubbedStats.timing, 'my-client.feedName.response_time');
-            });
-    });
+  it('increments a request counter with the name of the client and feed if provided', () => {
+    api.get('/').reply(200);
+
+    return Blackadder.createClient()
+      .get(url)
+      .use(stats(stubbedStats, 'my-client', 'feedName'))
+      .asBody()
+      .catch(assert.ifError)
+      .then(() => {
+        sinon.assert.calledWith(stubbedStats.increment, 'my-client.feedName.requests');
+      });
+  });
+
+  it('increments counter response for each response', () => {
+    api.get('/').reply(200);
+
+    return Blackadder.createClient()
+      .get(url)
+      .use(stats(stubbedStats, 'my-client', 'feedName'))
+      .asBody()
+      .catch(assert.ifError)
+      .then(() => {
+        sinon.assert.calledWith(stubbedStats.increment, 'my-client.feedName.responses.200');
+      });
+  });
 });
