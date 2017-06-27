95e704ab143d63a5a75d6d523c3e75d815507fe4
nick spragg <nick.spragg@bbc.co.uk>
2017-06-27 17:36:46 +0100

    Task: Export Context and Transport.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Client = require('./lib/client');
 const DEFAULT_TRANSPORT = require('./lib/transport/request');
+const Transport = require('./lib/transport/transport');
+const Context = require('./lib/context');
 
 module.exports.createClient = (httpTransport) => {
   return new Client(httpTransport || DEFAULT_TRANSPORT);
 };
+
+module.exports.transport = Transport;
+module.exports.transport = Context;
