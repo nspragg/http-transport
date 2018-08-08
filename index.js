9f0cd2948e113f263ca0845f4939a43f3173ccc4
nick spragg <5319732+nspragg@users.noreply.github.com>
2018-08-08 11:46:29 +0100

    Features: synced with bbc for type definitions.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const Client = require('./lib/client');
-const DEFAULT_TRANSPORT = require('./lib/transport/request');
+'use strict';
 
+const HttpTransportBuilder = require('./lib/builder');
+const DefaultTransport = require('./lib/transport/request');
+
+module.exports.defaultTransport = DefaultTransport;
+module.exports.builder = HttpTransportBuilder;
 module.exports.transport = require('./lib/transport/transport');
 module.exports.context = require('./lib/context');
-module.exports.toJson = require('./lib/plugins/asJson');
+module.exports.toJson = require('./lib/middleware/asJson');
+module.exports.logger = require('./lib/middleware/logger');
+module.exports.setContextProperty = require('./lib/middleware/setContextProperty');
 
-module.exports.createClient = (httpTransport) => new Client(httpTransport || this.getDefaultTransport());
+module.exports.createClient = () => {
+  return new HttpTransportBuilder(new DefaultTransport()).createClient();
+};
 
-module.exports.getDefaultTransport = () => {
-  return new DEFAULT_TRANSPORT;
+module.exports.createBuilder = (transport) => {
+  return new HttpTransportBuilder(transport || new DefaultTransport());
 };
